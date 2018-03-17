d66e8622ba3be48dd57c46093edfd67924cc29c8
Bernardo Dias <bernardodiasdacruz@gmail.com>
2018-03-17 20:39:33 -0300

    update metatags

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -9,33 +9,29 @@ class Meta extends Component {
   }
 
   render() {
+    const siteName = 'Bernardo Dias da Cruz'
     const title = this.props.title
-      ? `${this.props.title} - Bernardo Dias da Cruz`
-      : 'Bernardo Dias da Cruz'
+      ? `${this.props.title} - ${siteName}`
+      : siteName
     const description = this.props.description
-    const image = this.props.image
-      || 'http://bernardodiasdacruz.com/images/thumbnail.png'
-    const href = window.location.href
+    const image = this.props.image || '/images/thumbnail.png'
+    const pathname = window.location.pathname
     return (
       <Helmet>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <link rel="canonical" href={href} />
-
+        <link rel="canonical" href={pathname} />
         <meta name="twitter:site" content="@bernardodiasc" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:creator" content="@bernardodiasc" />
         <meta name="twitter:image" content={image} />
-
         <meta property="og:title" content={title} />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content={href} />
+        <meta property="og:url" content={pathname} />
         <meta property="og:image" content={image} />
-        <meta property="og:image:width" content="501" />
-        <meta property="og:image:height" content="384" />
         <meta property="og:description" content={description} />
-        <meta property="og:site_name" content="Bernardo Dias da Cruz" />
+        <meta property="og:site_name" content={siteName} />
       </Helmet>
     )
   }
