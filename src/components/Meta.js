67e9492aeea8efbc253220e9db9e9c9e9f7e5600
Bernardo Dias <bernardodiasdacruz@gmail.com>
2018-03-17 20:32:02 -0300

    update metatags

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,51 +1,40 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
-import config from 'config'
 
 class Meta extends Component {
+  static defaultProps = {
+    title: '',
+    description: '',
+    image: '',
+  }
+
   render() {
     const title = this.props.title
       ? `${this.props.title} - Bernardo Dias da Cruz`
       : 'Bernardo Dias da Cruz'
-    const description = this.props.description || ''
-    const image = this.props.image || `${config.PUBLIC_URL}/images/thumbnail.png`
-    const slug = this.props.slug || '/'
+    const description = this.props.description
+    const image = this.props.image
+      || 'http://bernardodiasdacruz.com/images/thumbnail.png'
+    const href = window.location.href
     return (
       <Helmet>
         <title>{title}</title>
-        {description !== '' && (
-          <meta name="description" content={description} />
-        )}
-        <link rel="canonical" href={`${config.PUBLIC_URL}${slug}`} />
+        <meta name="description" content={description} />
+        <link rel="canonical" href={href} />
 
-        {description !== '' && (
-          <meta name="twitter:card" content={description} />
-        )}
         <meta name="twitter:site" content="@bernardodiasc" />
         <meta name="twitter:title" content={title} />
-        {description !== '' && (
-          <meta name="twitter:description" content={description} />
-        )}
+        <meta name="twitter:description" content={description} />
         <meta name="twitter:creator" content="@bernardodiasc" />
-        {image !== '' && (
-          <meta
-            name="twitter:image"
-            content={`${config.PUBLIC_URL}${image}`}
-          />
-        )}
+        <meta name="twitter:image" content={image} />
 
         <meta property="og:title" content={title} />
         <meta property="og:type" content="article" />
-        <meta property="og:url" content={`http://bernardodiasdacruz.com${slug}`} />
-        {image !== '' && (
-          <meta
-            property="og:image"
-            content={`${config.PUBLIC_URL}${image}`}
-          />
-        )}
-        {description !== '' && (
-          <meta property="og:description" content={description} />
-        )}
+        <meta property="og:url" content={href} />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:width" content="501" />
+        <meta property="og:image:height" content="384" />
+        <meta property="og:description" content={description} />
         <meta property="og:site_name" content="Bernardo Dias da Cruz" />
       </Helmet>
     )
