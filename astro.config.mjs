1f17a48ff9df4157ce354a51217354a5eab1e9b2
Bernardo Dias <bernardodiasdacruz@gmail.com>
2026-02-19 18:05:56 -0300

    Revert custom components on posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
+import AutoImport from 'astro-auto-import';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://bernardodiasdacruz.com',
-  integrations: [sitemap(), mdx()],
+  integrations: [
+    AutoImport({
+      imports: [
+        './src/components/Video.astro',
+        './src/components/Codepen.astro',
+        './src/components/ImageFigure.astro',
+        './src/components/Animation.astro',
+        './src/components/SankeyChart.astro',
+      ],
+    }),
+    mdx(),
+    sitemap(),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
